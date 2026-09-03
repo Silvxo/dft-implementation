fix dft formula

main.py
@@ -7,20 +7,20 @@ def dft(nums):
     N = len(nums)
     for k in range(N):
         sum = 0.0
-        for n in nums:
-            sum += n * np.power(np.e, (2j * np.pi * k)/N)
+        for n, x_n in enumerate(nums):
+            sum += x_n * np.power(np.e, (-(2j * np.pi * k * n)/N))
         magnitude = math.hypot(sum.real, sum.imag)
         resulting_array.append(magnitude)
     return resulting_array
 
 
-Fs = 500;  # taxa de amostragem
+Fs = 100;  # taxa de amostragem
 Ts = 1.0/Fs; # periodo de amostragem
 t = np.arange(0,1,Ts) # vetor de tempo
 
-f = 15
+f = 5
 x1_n = np.sin(2*np.pi*f*t + 0)
-f = 20;
+f = 10;
 x2_n = np.sin(2*np.pi*f*t + 0)
 
 x_n = x1_n + x2_n
@@ -35,12 +35,16 @@ X = dft(x_n)
 X = X[:int(n/2)] # apenas um lado
 
 
-print(X)
+# print(X)
 
 fig, ax = plt.subplots(2, 1)
 ax[0].plot(t,x_n)
 ax[0].set_xlabel('Tempo')
 ax[0].set_ylabel('Amplitude')
+
 ax[1].plot(frq,X,'r')
 ax[1].set_xlabel('Freq (Hz)')
 ax[1].set_ylabel('|X(freq)|')
+
+plt.tight_layout()
+plt.show()